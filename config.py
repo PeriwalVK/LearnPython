09cd70b588f6061e644c7f8112dd8055ed34db8e
Vikas Periwal <vkperiwal97@gmail.com>
2026-02-01 15:19:52 +0530

    more ways to calc base_dir

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-# import os 
-
-# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-# # print(BASE_DIR)
-
+import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent #.parent
+# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BASE_DIR = Path(__file__).resolve().parent
 print(BASE_DIR)
