f776f89814f9588a74f8197a149580e66216c020
Vikas Periwal <vkperiwal97@gmail.com>
2026-02-14 17:09:21 +0530

    deleted bots

--- bots/camoufox_bot.py
+++
@@ -1,6 +0,0 @@
-from camoufox.sync_api import Camoufox
-
-with Camoufox(headless=False) as browser:
-    page = browser.new_page()
-    page.goto("https://pixelscan.net/bot-check")
-    page.wait_for_timeout(5000)
--- bots/constants/driver_configuration.py
+++
@@ -1,6 +0,0 @@
-
-class WAIT_TIMES: # seconds
-    
-    DRIVER_DEFAULT = 15
-    
-    POP_UP_WAIT = 10
