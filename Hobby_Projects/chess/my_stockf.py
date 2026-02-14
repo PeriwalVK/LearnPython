80d888e2fc29a6763d66b961df4c8c82c856bf72
Vikas Periwal <vkperiwal97@gmail.com>
2026-02-14 17:20:52 +0530

    deleted hobby projects folder

--- Hobby_Projects/chess/my_stockf.py
+++
@@ -1,8 +0,0 @@
-# from stockfish import Stockfish
-# from pathlib import Path
-
-# BASE_DIR = Path(__file__).resolve().parent #.parent
-# print(BASE_DIR)
-
-# stockfish = Stockfish(path=BASE_DIR)
-# stockfish.get_stockfish_major_version()
