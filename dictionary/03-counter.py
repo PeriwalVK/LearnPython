b1d70ea78036d0322ef01eb278ea52005fdde334
Vikas Periwal <vkperiwal97@gmail.com>
2026-02-25 02:31:13 +0530

    minor refactoring

--- dictionary/03-counter.py
+++ dictionary/03-counter.py
@@ -1,15 +1,17 @@
 from collections import Counter
 
 
-c1 = Counter({"a": 5, "b": 3, "c": 2})
-c2 = Counter({"a": 2, "b": 4, "d": 1})
+c1 = Counter({"a": 5, "b": 3, "c": 2, "e": 5})
+c2 = Counter({"a": 2, "b": 4, "d": 1, "e": 5})
 c1.subtract(c2)
-print(c1)
-# Output: Counter({'a': 3, 'c': 2, 'b': -1, 'd': -1})
+# Allows zero and negative counts
+# original c1 modified
+print(c1)  # Counter({'a': 3, 'c': 2, 'e': 0, 'b': -1, 'd': -1})
 
 
-c1 = Counter({"a": 5, "b": 3, "c": 2})
-c2 = Counter({"a": 2, "b": 4, "d": 1})
+c1 = Counter({"a": 5, "b": 3, "c": 2, "e": 5})
+c2 = Counter({"a": 2, "b": 4, "d": 1, "e": 5})
 result = c1 - c2
-print(result)
-# Output: Counter({'a': 3, 'c': 2})
+# Removes zero and negative counts
+# original c1 not modified
+print(result)  # Counter({'a': 3, 'c': 2})
